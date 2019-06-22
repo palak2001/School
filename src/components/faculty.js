dfab80806d4bba7fd93f066441c6d6adb37f1263
palak2001 <iit2018117@iiita.ac.in>
2019-06-22 16:02:20 +0530

    My school website

--- src/components/faculty.js
+++ src/components/faculty.js
@@ -1,34 +1,85 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-
+import './bgfaculty.jpg';
+import './stylesheet.css';
 class Faculty extends Component
 {
     render()
     {
         return(
-            <div>
-                <div className="container">
-                    <img src="#" className="rounded" alt="" />
-                    <div>Info of Teacher</div>
+            <div className="bgfaculty">
+                <h1 className="text">Faculty</h1>
+                <div className="aln">
+                    <div class="card text-white bg-dark mb-3">
+                        <img class="card-img-top" src="..." alt="Card image cap" />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                    <div class="card text-white bg-dark mb-3">
+                        <img class="card-img-top" src="..." alt="Card image cap" />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
                 </div>
-                <div className="container">
-                    <img src="#" className="rounded" alt="" />
-                    <div>Info of Teacher</div>
+                <div className="aln">
+                    <div class="card bg-light mb-3">
+                        <img class="card-img-top" src="..." alt="Card image cap" /> 
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                    <div class="card bg-light mb-3">
+                        <img class="card-img-top" src="..." alt="Card image cap" /> 
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                    <div class="card bg-light mb-3">
+                        <img class="card-img-top" src="..." alt="Card image cap" /> 
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
                 </div>
-                <div className="container">
-                    <img src="#" className="rounded" alt="" />
-                    <div>Info of Teacher</div>
-                </div>
-                <div className="container">
-                    <img src="#" className="rounded" alt="" />
-                    <div>Info of Teacher</div>
-                </div>
-                <div className="container">
-                    <img src="#" className="rounded" alt="" />
-                    <div>Info of Teacher</div>
+                <div className="aln">
+                    <div class="card bg-light mb-3">
+                        <img class="card-img-top" src="..." alt="Card image cap" />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                    <div class="card bg-light mb-3">
+                        <img class="card-img-top" src="..." alt="Card image cap" />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                    <div class="card bg-light mb-3">
+                        <img class="card-img-top" src="..." alt="Card image cap" />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
